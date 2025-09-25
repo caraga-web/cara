6057ded8e1b6003b574d14a46dee80215c5226bd
caraga-web <caragaweb@tesda.gov.ph>
2025-09-25 11:32:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 import time
 
+def chatbot_response(user_message: str) -> str:
+    user_message = (user_message or "").lower().strip()
+
+    if user_message in ["hi","hello","start","hey"]:
+      
